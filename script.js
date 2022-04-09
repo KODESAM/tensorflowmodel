c888b2dc7ef562559b6c153f0a9e1adb7e46b4c9
Glitch (glitch-in-bio) <none>
2022-04-09 16:18:30 +0000

    🏥📕 Checkpoint ./index.html:43708397/2857 ./public/styles/style.css:43708397/7045 ./script.js:43708397/9344

---
+++ script.js
@@ -0,0 +1,41 @@
+const STATUS = document.getElementById('status');
+const VIDEO = document.getElementById('webcam');
+const ENABLE_CAM_BUTTON = document.getElementById('enableCam');
+const RESET_BUTTON = document.getElementById('reset');
+const TRAIN_BUTTON = document.getElementById('train');
+const MOBILE_NET_INPUT_WIDTH = 224;
+const MOBILE_NET_INPUT_HEIGHT = 224;
+const STOP_DATA_GATHER = -1;
+const CLASS_NAMES = [];
+
+ENABLE_CAM_BUTTON.addEventListener('click', enableCam);
+TRAIN_BUTTON.addEventListener('click', trainAndPredict);
+RESET_BUTTON.addEventListener('click', reset);
+
+
+function enableCam() {
+  // TODO: Fill this out later in the codelab!
+}
+
+
+function trainAndPredict() {
+  // TODO: Fill this out later in the codelab!
+}
+
+
+function reset() {
+  // TODO: Fill this out later in the codelab!
+}
+
+let dataCollectorButtons = document.querySelectorAll('button.dataCollector');
+for (let i = 0; i < dataCollectorButtons.length; i++) {
+  dataCollectorButtons[i].addEventListener('mousedown', gatherDataForClass);
+  dataCollectorButtons[i].addEventListener('mouseup', gatherDataForClass);
+  // Populate the human readable names for classes.
+  CLASS_NAMES.push(dataCollectorButtons[i].getAttribute('data-name'));
+}
+
+
+function gatherDataForClass() {
+  // TODO: Fill this out later in the codelab!
+}
